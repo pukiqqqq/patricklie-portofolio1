87a67ae1cfead2ca25bc9c7a81244d569fe9bc10
pukiqqqq <patricklie335@gmail.com>
2026-03-04 17:01:58 +0700

    Updated HomePage.tsx

--- patricklie-portofolio-main/src/pages/HomePage.tsx
+++ patricklie-portofolio-main/src/pages/HomePage.tsx
@@ -1,5 +1,6 @@
 import Hero from '../components/Hero';
 import Skills from '../components/Skills';
+import Experience from '../components/Experience';
 import FeaturedProjects from '../components/FeaturedProjects';
 import Contact from '../components/Contact';
 
@@ -8,6 +9,7 @@ export default function HomePage() {
     <div>
       <Hero />
       <Skills />
+      <Experience />
       <FeaturedProjects />
       <Contact />
     </div>
